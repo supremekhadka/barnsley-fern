cf289a4b1d238ddba3e95b95109290188abaaf41
Supreme Khadka <supremekhadka2@gmail.com>
2024-11-04 22:05:12 +0545

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <main className="flex flex-col min-h-screen w-full justify-center items-center">
+        Barnsley Fern Fractals
+      </main>
+    </>
+  );
+};
+
+export default page;
